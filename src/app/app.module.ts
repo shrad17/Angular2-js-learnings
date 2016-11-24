c9d73ee7ebba0ccb73b52fcfb44ad48e8bf4d71f
shrad17 <s.m.pandya174@gmail.com>
2016-11-23 22:52:56 -0500

    created new component plan trip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlanTripComponent } from './plan-trip/plan-trip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanTripComponent
   ],
   imports: [
     BrowserModule,
